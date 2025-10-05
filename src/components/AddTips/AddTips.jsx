3aa5d9b3a6e814a25fa6cb9e7ce03104bafa8959
Eunji Lee <lee00866@algonquinlive.com>
2025-10-05 11:48:06 -0400

    init commit

---
+++ src/components/AddTips/AddTips.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AddTips() {
+  return (
+    <div>
+      <form>
+        <div>
+          <label for="cash">(1)Cash Tips</label>
+          <input type="text" id="" name="" />
+        </div>
+      </form>
+    </div>
+  );
+}
